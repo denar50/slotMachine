a15780490fafec55f751c904afe1a442a19ef390
Edgar Fabricio Santos Rincon <=>
2016-10-02 23:22:36 +0200

    Basic styles added

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ window.onload = () => {
 	const slotElements = []
 	document.querySelectorAll('.slot-machine__slot').forEach(element => slotElements.push(element))
 	const slotMachine = new SlotMachine(slotElements)
-	document.querySelector('.play-btn').addEventListener('click', (event) => {
+	document.querySelector('.controls__play-btn').addEventListener('click', (event) => {
 		const { target: button } = event
 		slotMachine.play().then(() => {
 			button.disabled = false
